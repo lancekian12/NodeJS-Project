fee73e7ffd7f681526214f6b847b6260fae4196c
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-06-16 04:07:48 +0800

    Handling Post Request

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res.status(200).json({ message: 'Hello from the server side!' });
 // });
@@ -21,7 +23,25 @@ app.get('/api/v1/tours', (req, res) => {
     },
   });
 });
-app.post('/api/v1/tours', (req, res) => {});
+app.post('/api/v1/tours', (req, res) => {
+  //   console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
 
 const port = 3000;
 app.listen(port, () => {
