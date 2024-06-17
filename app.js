63c58972eb1781b11bccb2f539676b9dab6785ef
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-06-17 23:40:55 +0800

    Handling Delete Request

--- app.js
+++ app.js
@@ -43,18 +43,16 @@ app.get('/api/v1/tours/:id', (req, res) => {
   });
 });
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+app.delete('/api/v1/tours/:id', (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
       message: 'Invalid ID',
     });
   }
-  res.status(200).json({
+  res.status(204).json({
     status: 'success',
-    data: {
-      tour: '<Updated tour here....',
-    },
+    data: null,
   });
 });
 
