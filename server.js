b37599c460223b97bc63c73cb5ab4f0262f09290
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-06-22 14:54:18 +0800

    connect the mongodb database

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
-// console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
-const port = process.env.PORT || 3000;
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connection successful!'));
+
+const port = process.env.port || 3000;
 app.listen(port, () => {
-  console.log(`App running on port ${port}....`);
+  console.log(`App is running on port ${port}...`);
 });
