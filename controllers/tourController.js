f7e5108d8f24a74ddd9a7d12c08cdf6542451544
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-06-26 01:54:55 +0800

    Added API Feature Aliasing

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,6 +5,13 @@ const Tour = require('./../models/tourModel');
 //   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 // );
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     console.log(req.query);
