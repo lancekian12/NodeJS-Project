46a91dfc0fa0b59a68efe7b673e3242c92fd4202
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-07-01 00:54:32 +0800

    Created unhandled routes

--- app.js
+++ app.js
@@ -13,11 +13,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -26,6 +21,13 @@ app.use((req, res, next) => {
 // Mounting middleware
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`
+  });
+});
 
 // 4.) START SERVER
 module.exports = app;
