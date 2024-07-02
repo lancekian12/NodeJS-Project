b2f338d14a1e0cd10f89b652d83abeed267cc513
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-07-02 23:27:01 +0800

    created validator for password and confirmPassword also encrypted it

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -26,14 +27,26 @@ const userSchema = new mongoose.Schema({
   },
   confirmPassword: {
     type: String,
-    required: [true, 'A user must have a confirm password']
-    // validate: {
-    //   validator: function(val) {
-    //     return val === this.password;
-    //   },
-    //   message: `Confirm password should be equal to password`
-    // }
+    required: [true, 'A user must have a confirm password'],
+    validate: {
+      validator: function(val) {
+        return val === this.password;
+      },
+      message: `Password are not the same!`
+    }
   }
+});
+
+userSchema.pre('save', async function(next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete confirmPassword field
+  this.confirmPassword = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
