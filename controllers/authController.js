d232876f62a5e12ee3e4b5357b160bc140182c6e
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-07-06 00:26:39 +0800

    Added forgotPassword route and resetPassword route

--- controllers/authController.js
+++ controllers/authController.js
@@ -105,3 +105,16 @@ exports.restrictTo = (...roles) => {
     next();
   };
 };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on resetToken
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) next(new AppError('There is no user with email address'), 404);
+
+  // 2) Generate the random reset token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+
+  // 3) Send it to the user's email
+});
+exports.resetPassword = (req, res, next) => {};
