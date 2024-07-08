63f8fce67045dbf8621133143f5861e1e33ca62f
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-07-09 06:14:20 +0800

    Sending JWT via Cookie

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const morgan = require('morgan');
-
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -8,10 +7,11 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1.) MIDDLEWARE
+// 1.) GLOBAL MIDDLEWARE
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
