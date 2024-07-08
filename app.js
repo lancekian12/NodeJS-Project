8ca5c6ac05d3694096e80b293239bbdb3d53b744
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-07-09 06:15:23 +0800

    Implement rate limiting or max request per IP address

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -11,6 +13,13 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 3,
+  windowMs: 60 * 60 * 1000, // 1 hour
+  message: 'Too many request from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
