0d879a9fcd033e049d087a0caa2f2332e61f7471
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-07-09 06:20:57 +0800

    Setting security HTTP Headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,10 +11,15 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1.) GLOBAL MIDDLEWARE
+// Set Security HTTP Header
+app.use(helmet());
+
+// Development Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit request from same API
 const limiter = rateLimit({
   max: 3,
   windowMs: 60 * 60 * 1000, // 1 hour
@@ -21,9 +27,13 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body Parser, reading data from the body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files from the public folder
 app.use(express.static(`${__dirname}/public`));
 
+// Test Middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
